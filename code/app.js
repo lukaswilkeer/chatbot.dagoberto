e1ef66982427d217efd50fe5d1f665d153a43431
Jorge Rafael <jorgerafael@uxer.com.br>
2017-02-17 21:11:12 -0200

    first commit - dando vida ao dagoberto

---
+++ code/app.js
@@ -0,0 +1,55 @@
+(() => {
+
+const TelegramBot = require('node-telegram-bot-api');
+
+
+const TOKEN_TELEGRAM = `` ;
+// token gerado pelo @bot_father
+
+// Dagoberto o Bot
+// http://t.me/dabobertoobot
+
+/**
+ * This example demonstrates setting up a webook, and receiving
+ * updates in your express app
+ */
+
+const TOKEN = process.env.TELEGRAM_TOKEN || '299692951:AAGgYFsiu57O2Th84QPb58aIB21eohdQvEI';
+const url = 'https://intense-wave-42330.herokuapp.com/';
+const port = process.env.PORT || 5000;
+
+const TelegramBot = require('..');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
+
+
+
+
+})()
