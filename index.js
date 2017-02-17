c2f1d571b5b179a9072ec491cd7e3d4f5584a4fc
Jorge Rafael <jorgerafael@uxer.com.br>
2017-02-17 21:23:11 -0200

    Tentando dar vida ao dagoberto

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 (() => {
 
 const TelegramBot = require('node-telegram-bot-api');
-
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const TOKEN_TELEGRAM = `` ;
 // token gerado pelo @bot_father
@@ -18,9 +19,7 @@ const TOKEN = process.env.TELEGRAM_TOKEN || '299692951:AAGgYFsiu57O2Th84QPb58aIB
 const url = 'https://intense-wave-42330.herokuapp.com/';
 const port = process.env.PORT || 5000;
 
-const TelegramBot = require('..');
-const express = require('express');
-const bodyParser = require('body-parser');
+
 
 // No need to pass any parameters as we will handle the updates with Express
 const bot = new TelegramBot(TOKEN);
@@ -45,14 +44,37 @@ app.listen(port, () => {
 });
 
 // Just to ping!
-bot.on('message', msg => {
-  bot.sendMessage(msg.chat.id, 'I am alive!');
-});
+
 
 app.get('/dev', (req,resp) => {
  resp.json({ dagoberto : "Está vivo "})
 })
 
 
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+// bot.on('message', (msg) => {
+//   const chatId = msg.chat.id;
+
+//   // send a message to the chat acknowledging receipt of their message
+//   bot.sendMessage(chatId, 'Received your message');
+// });
+
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
 
 })()
