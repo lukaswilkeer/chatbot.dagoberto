ee69a92d577c90c9407da6362ba05d8287076af0
Jorge Rafael <jorgerafael@uxer.com.br>
2017-02-22 00:47:57 -0300

    in progress

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ bot.on('text', msg => {
 
 })
 
+bot.on(['*', '/*'], (msg, self) => {
+  let id = msg.chat.id;
+  let reply = msg.message_id;
+  let type = self.type;
+  let parse = 'html';
+  return bot.sendMessage(
+    id, `This is a <b>${ type }</b> message.`, { reply, parse }
+  );
+});
+
 
 
 bot.connect();
